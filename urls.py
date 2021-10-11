534407b4edd37184162c20120e448bab9d0402a0
shivani tomar <80258536+shivi8827@users.noreply.github.com>
2021-10-11 13:47:17 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,20 +1,35 @@
+"""shoppingsite URL Configuration
 
-from django.urls import path
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
+
+
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+#from . import views
 
 urlpatterns = [
-    path("", views.index, name="ShopHome"),
-    path("about/", views.about, name="AboutUs"),
-    path("contact/", views.contact, name="ContactUs"),
-    path("tracker/", views.tracker, name="TrackingStatus"),
-    path("search/", views.search, name="Search"),
-    path("products<int:myid>/", views.productview, name="Productview"),
-    path("checkout/", views.checkout, name="Checkout"),
-        
+    path('admin/', admin.site.urls),
+    path('shop/', include('shop.urls')),
+    
+    #path('', views.index )
 
-    path("home/", views.home, name="Home"),
-    path("signup/", views.signup, name="Signup"),
-    path("login/",views.login, name="Login"),
-    path("logout/", views.logout, name="Logout")
-        
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
